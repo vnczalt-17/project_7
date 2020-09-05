a345d92614e140e90c6bc1d306a63514fad59af3
callahan-cw <callahan@clarusway.com>
2020-09-05 12:57:00 -0400

    added build script

---
+++ devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/build-script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
