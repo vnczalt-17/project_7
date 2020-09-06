ad89b227e0806acb846e92760f83bef131e5aa4b
callahan-cw <callahan@clarusway.com>
2020-09-06 12:06:54 -0400

    updated build script

--- devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/build-script.sh
+++ devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/build-script.sh
@@ -8,4 +8,14 @@ usermod -a -G docker ec2-user
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+TOKEN="xxxxxxxxxxx"
+FOLDER="https://$TOKEN@raw.githubusercontent.com/clarusway/clarusway-aws-devops-1-20/master/devops/projects/202-dockerization-bookstore-api-on-python-flask-mysql/"
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/bookstore-api.py" -L "$FOLDER"bookstore-api.py
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
+cd /home/ec2-user/bookstore-api
+docker build -t callahanclarus/bookstore-api:latest .
+docker-compose up -d
+
 
